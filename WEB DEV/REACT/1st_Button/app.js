71e59941c506b46d43ac3491b075abba9ff0f523
MILAN SINGHAL <118826647+milansinghal2004@users.noreply.github.com>
2024-05-17 07:06:18 +0000

    Updates!

---
+++ WEB DEV/REACT/1st_Button/app.js
@@ -0,0 +1,15 @@
+function MyButton(){
+    return(
+        <button>
+            I am a Button!
+        </button>
+    )
+}
+
+export default function MyApp(){
+    return(
+        <div>
+            <h1>Welcome to my First React App!</h1>
+        </div>
+    )
+}
